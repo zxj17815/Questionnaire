5bd8846eb108ae3388c1e8a85246655036d342d3
iceiceice <597952291@qq.com>
2021-08-14 21:13:24 +0800

    fix runing path

--- gunicorn.py
+++ gunicorn.py
@@ -19,7 +19,7 @@ import multiprocessing
 
 bind = '127.0.0.1:8005'  # 绑定ip和端口号
 backlog = 512  # 监听队列
-chdir = "/home/jay/fastapi"  # gunicorn要切换到的目的工作目录
+chdir = "/root/python/Questionnaire"  # gunicorn要切换到的目的工作目录
 timeout = 30  # 超时
 worker_class = 'uvicorn.workers.UvicornWorker'  # 使用gevent模式，还可以使用sync 模式，默认的是sync模式
 workers = multiprocessing.cpu_count() * 2 + 1  # 进程数
